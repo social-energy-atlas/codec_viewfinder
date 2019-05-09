f991686e8cd8af2b3fd9eadb8c8199c3f66d5213
jacquehettel <jacque.tidwell@gmail.com>
2019-05-08 20:04:34 -0400

    Added county taxonomy fields.

---
+++ sites/default/files/php/twig/5cd35db2653bb_feeds_feed.html.twig_qcTrpHAZXshtcn9888YC8B7yn/rlWAkJ-_zekprlv4YhhnYDI5U6rvBy3jRmnKnLbh3c4.php
@@ -0,0 +1,105 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+
+/* modules/contrib/feeds/templates/feeds_feed.html.twig */
+class __TwigTemplate_7b85f842aa6608df0394b273ad4a0f229ffdbcd8ac5d9b7e1b582228296cf451 extends \Twig\Template
+{
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = [
+        ];
+        $this->sandbox = $this->env->getExtension('\Twig\Extension\SandboxExtension');
+        $tags = ["set" => 2];
+        $filters = ["clean_class" => 4, "escape" => 10];
+        $functions = [];
+
+        try {
+            $this->sandbox->checkSecurity(
+                ['set'],
+                ['clean_class', 'escape'],
+                []
+            );
+        } catch (SecurityError $e) {
+            $e->setSourceContext($this->getSourceContext());
+
+            if ($e instanceof SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        // line 2
+        $context["classes"] = [0 => "feeds_feed", 1 => ("feeds_feed--feed-type-" . \Drupal\Component\Utility\Html::getClass($this->sandbox->ensureToStringAllowed($this->getAttribute(        // line 4
+($context["feed"] ?? null), "bundle", [])))), 2 => (($this->getAttribute(        // line 5
+($context["feed"] ?? null), "isActive", [], "method")) ? ("feeds_feed-active") : ("")), 3 => ((        // line 6
+($context["view_mode"] ?? null)) ? (("feeds_feed--view-mode-" . \Drupal\Component\Utility\Html::getClass($this->sandbox->ensureToStringAllowed(($context["view_mode"] ?? null))))) : (""))];
+        // line 9
+        echo "
+<article";
+        // line 10
+        echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed($this->getAttribute(($context["attributes"] ?? null), "addClass", [0 => ($context["classes"] ?? null)], "method")), "html", null, true);
+        echo ">
+  <div";
+        // line 11
+        echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed($this->getAttribute(($context["content_attributes"] ?? null), "addClass", [0 => "feeds_feed__content"], "method")), "html", null, true);
+        echo ">
+    ";
+        // line 12
+        echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["content"] ?? null)), "html", null, true);
+        echo "
+  </div>
+</article>
+";
+    }
+
+    public function getTemplateName()
+    {
+        return "modules/contrib/feeds/templates/feeds_feed.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  71 => 12,  67 => 11,  63 => 10,  60 => 9,  58 => 6,  57 => 5,  56 => 4,  55 => 2,);
+    }
+
+    /** @deprecated since 1.27 (to be removed in 2.0). Use getSourceContext() instead */
+    public function getSource()
+    {
+        @trigger_error('The '.__METHOD__.' method is deprecated since version 1.27 and will be removed in 2.0. Use getSourceContext() instead.', E_USER_DEPRECATED);
+
+        return $this->getSourceContext()->getCode();
+    }
+
+    public function getSourceContext()
+    {
+        return new Source("", "modules/contrib/feeds/templates/feeds_feed.html.twig", "/Applications/MAMP/htdocs/codec_viewfinder/modules/contrib/feeds/templates/feeds_feed.html.twig");
+    }
+}
