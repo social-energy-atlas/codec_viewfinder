844d1ef5df0985d5645d2b945b708c2742af6615
Jacqueline Hettel <jacque@jacquelinesipro.attlocal.net>
2019-05-13 09:09:29 -0400

    Updated modules and imported views.

--- sites/default/settings.php
+++ sites/default/settings.php
@@ -778,7 +778,7 @@ $settings['entity_update_backup'] = TRUE;
 #   include $app_root . '/' . $site_path . '/settings.local.php';
 # }
 $databases['default']['default'] = array (
-  'database' => 'drupaltest3',
+  'database' => 'drupal0513',
   'username' => 'admin',
   'password' => 'admin',
   'prefix' => '',
